7500c26658f9c8df048496f0c0f2220676592da0
Ardi12345X <77550679+Ardi12345X@users.noreply.github.com>
2021-01-17 03:07:07 +0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,39 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f hecked -F By. M3R4Z
+echo $red"selamat datang"
+echo
+sleep 2
+echo $green"silahkan pilih tools nya :"
+echo
+echo $cyan"1.) stabilkan jaringan"
+echo
+echo $blue"0.)keluar aja !!!"
+echo
+echo $white
+read -p "silahkan pilih :" bro
+if [ $bro =1 ] ||[ $bro = 1 ]
+then
+clear
+toilet -f hecked -f By. M3R4Z
+echo $green"stabilkan jaringan  hecked.by MR M3R4Z"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+echo $yellow
+figlet "Bye"
+echo $white"To Be Continued"
+sleep3
+exit
+fi
